1d90dd95cfbbabda076fe765ef3cdf82f72b796d
Tiến Hiệu <duongnguyen1216@gmail.com>
2018-10-31 15:19:16 +0700

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Web widget many2many_binary_thumbnail",
+    'version': '0.1',
+    'author': 'Entrust Developa',
+    'maintainer': 'Entrust Developa',
+    'website': "http://entrustlab.com",
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'sequence': 100,
+    'description': """Web widget many2many_binary_thumbnail""",
+    'summary': """Web widget many2many_binary_thumbnail""",
+    'depends': ['base'],
+    'data': ['views/assets.xml'],
+    'demo': [],
+    'qweb': ['static/src/xml/*.xml'],
+    # 'js': ['static/src/js/first_module.js',],
+    # 'css': ['static/src/css/web_example.css',],
+    # 'images': ['static/description/icon.png',],
+    'auto_install': False,
+    'application': False,
+    'installable': True,
+    # 'external_dependencies': {'python' : ['usb.core','serial','qrcode']}
+    # 'pre_init_hook': 'pre_init_hook',
+    # 'post_init_hook': 'post_init_hook',
+    # 'uninstall_hook': 'uninstall_hook',
+}
---
+++ static/src/js/field_binary_file_upload_thumbnails.js
@@ -0,0 +1,32 @@
+odoo.define('web.field_binary_file_upload_thumbnails', function(require) {
+"use strict";
+
+    var relational_fields = require('web.relational_fields');
+    var registry = require('web.field_registry');
+    var core = require('web.core');
+    var qweb = core.qweb;
+
+    var FieldMany2ManyBinaryMultiFilesWithThumbnail = relational_fields.FieldMany2ManyBinaryMultiFiles.extend({
+        _render: function() {
+        debugger;
+            this._super.apply(this, arguments);
+            this.$('.oe_placeholder_files, .oe_attachments')
+                .replaceWith($(qweb.render('FieldBinaryFileUploader.thumbnails', {
+                    widget: this,
+                })));
+            this.$('.o_image[data-mimetype^="image"]').each(function() {
+                var $img = $(this);
+                if (/gif|jpe|jpg|png/.test($img.data('mimetype')) && $img.data('src')) {
+                    $img.css('background-image', "url('" + $img.data('src') + "')");
+                }
+            });
+        },
+    });
+
+    registry.add('many2many_binary_thumbnail', FieldMany2ManyBinaryMultiFilesWithThumbnail)
+
+    return {
+        FieldMany2ManyBinaryMultiFilesWithThumbnail: FieldMany2ManyBinaryMultiFilesWithThumbnail,
+    };
+
+});
