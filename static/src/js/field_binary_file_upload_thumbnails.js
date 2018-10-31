810b28bde163441545b3e6e6981540b8187f0dbf
Tiến Hiệu <duongnguyen1216@gmail.com>
2018-10-31 15:35:04 +0700

    [FIX] remove some unneccessary code

--- static/src/js/field_binary_file_upload_thumbnails.js
+++ static/src/js/field_binary_file_upload_thumbnails.js
@@ -8,7 +8,6 @@ odoo.define('web.field_binary_file_upload_thumbnails', function(require) {
 
     var FieldMany2ManyBinaryMultiFilesWithThumbnail = relational_fields.FieldMany2ManyBinaryMultiFiles.extend({
         _render: function() {
-        debugger;
             this._super.apply(this, arguments);
             this.$('.oe_placeholder_files, .oe_attachments')
                 .replaceWith($(qweb.render('FieldBinaryFileUploader.thumbnails', {
